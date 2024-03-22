1bf353ff8ab2f55dfbb6d4e7367cd876ea96c172
pangil5634 <oksk6681@naver.com>
2024-03-22 20:16:51 +0900

    style : white / black color add

--- pangil5634_introduce/src/Style/theme.js
+++ pangil5634_introduce/src/Style/theme.js
@@ -7,6 +7,7 @@ export const theme = {
     green_100: "#B2E3D4",
 
     black_100: "#3D3B40",
+    white_100: "#ffffff",
     blueDragon_100: "#525CEB",
     gray_100: "#BFCFE7",
     pink_50:"#F8EDFF"
